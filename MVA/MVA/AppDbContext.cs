711f81808f4cfc21981d1f6ed7193113a6e097ff
Fernando Jesus <fernando.js8@outlook.com>
2018-10-03 20:19:33 -0300

    Customized Index, and created the create page

---
+++ MVA/MVA/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MVA
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options)
+            : base(options) {}
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ MVA/MVA/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVA
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+
+        [Required, StringLength(49)]
+        public string Name { get; set; }
+
+
+    }
+}
